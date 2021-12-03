9e776735b7faf6dc517522f03782b9810b50850a
Ben C <bwc9876@gmail.com>
2021-12-03 16:06:13 +0000

    Continue Day 3

--- day_3/diagnostics.py
+++ day_3/diagnostics.py
@@ -1,4 +1,6 @@
 
+
+
 def bin_to_dec(input_bin: str):
   output_dec = 0
   for index, number in enumerate(reversed(input_bin)):
@@ -9,20 +11,21 @@ def bin_to_dec(input_bin: str):
 def get_counts(input_bins):
   counts = [[0, 0] for x in range(len(input_bins[0]))]
   for byte in input_bins:
-    for index, bit in enumerate(byte):
+    print("Line: ", input_bins.index(byte))
+    for index, bit in enumerate(byte[:-1]):
       counts[index][int(bit)] += 1
   return counts
 
 def contruct_gamma_rate(counts):
   output_bin = ""
   for pair in counts:
-    output_bin += "0" if output_bin[0] > output_bin[1] else "1"
+    output_bin += "0" if pair[0] > pair[1] else "1"
   return output_bin, bin_to_dec(output_bin)
 
 def construct_epsilon_rate(counts):
   output_bin = ""
   for pair in counts:
-    output_bin += "0" if output_bin[0] < output_bin[1] else "1"
+    output_bin += "0" if pair[0] < pair[1] else "1"
   return output_bin, bin_to_dec(output_bin)  
 
 def get_rates(input_bytes):
@@ -34,3 +37,9 @@ def get_rates(input_bytes):
   print("Decimal Gamma: ", dec_gamma)
   print("Decimal Epsilon: ", dec_epsilon)
   print("Power Consumption Rate: ", dec_epsilon * dec_gamma)
+
+if __name__ == "__main__":
+  input_list = []
+  with open("3.txt", 'r') as file:
+    input_list = file.readlines()
+  get_rates(input_list)
